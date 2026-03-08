fa5aa9720213284bd8897c72b25c302f976bfabe
ProgrammerLP <developer.programmerlp@gmx.net>
2026-03-08 09:55:39 +0100

    lang and utils

---
+++ common/src/main/java/net/adeptstack/cms/Utils.java
@@ -0,0 +1,37 @@
+package net.adeptstack.cms;
+
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.DyeItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class Utils {
+
+    public static ResourceLocation id(String ns, String path) {
+        return new ResourceLocation(ns, path);
+    }
+
+    public static DyeColor colorFromItem(ItemStack stack) {
+        if (stack.getItem() instanceof DyeItem) {
+            return ((DyeItem)stack.getItem()).getDyeColor();
+        } else {
+            return null;
+        }
+    }
+
+    public static VoxelShape rotateShape(Direction from, Direction to, VoxelShape shape) {
+        VoxelShape[] buffer = new VoxelShape[]{ shape, Shapes.empty() };
+
+        int times = (to.ordinal() - from.get2DDataValue() + 4) % 4;
+        for(int i = 0; i < times; i++) {
+            buffer[0].forAllBoxes((minX, minY, minZ, maxX, maxY, maxZ) -> buffer[1] = Shapes.or(buffer[1], Shapes.create(1 - maxZ, minY, minX, 1 - minZ, maxY, maxX)));
+            buffer[0] = buffer[1];
+            buffer[1] = Shapes.empty();
+        }
+
+        return buffer[0];
+    }
+}
